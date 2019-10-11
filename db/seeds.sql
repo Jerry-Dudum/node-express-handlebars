2cc8b478c7655d06159082057e2ba0ab08fd9dd0
Jerry Dudum <jsdudum@gmail.com>
2019-10-10 18:04:56 -0700

    added connection.js to link to movies_db

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS movies_db;
 CREATE DATABASE movies_db;
 USE movies_db;
 
-CREATE TABLE moveis
+CREATE TABLE movies
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
